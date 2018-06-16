682697f9bf5a1bea5a3b1104d53436934343f147
Sam <samong0105@gmail.com>
2018-06-17 00:02:16 +1200

    Changed layout from using pack to using grid. Added checkboxes for different password options.

--- prototype.py
+++ prototype.py
@@ -3,7 +3,7 @@ import string
 from random import *
 
 smallFont = ["Courier", 14]
-mediumFont = ["Avenir", 16]
+avenir = ["Avenir", 14]
 bgColor = "#FAF6F6"
 textColor = "#000000"
 
@@ -17,33 +17,47 @@ def showPassword():
         pwBox.pack()
     except ValueError:
         pwBox.configure(text = "Input has to be a number. Try again")
-        pwBox.pack()
+        pwBox.grid(row = 5)
 
 
 #Create GUI for a window
 window = Tk()
 window.geometry("400x400")
 window.resizable(0,0)
-window.pack_propagate(0)
+# window.pack_propagate(0)
 
 window.title("Password generator")
+upperCase = IntVar()
+upperCaseCheckBox = Checkbutton(window, text = "UpperCase letter", variable = upperCase)
+
+number = IntVar()
+numberCheckBox = Checkbutton(window, text = "Number: 1234567890", variable = number)
+
+symbol = IntVar()
+symbolCheckBox = Checkbutton(window, text = "Symbol: !@#$%^&*()_-+=", variable = symbol)
+
 label = Label(window, text = "Length of password:")
 entry = Entry(window)
 btn = Button(window, text ="Generate", command = showPassword)
 pwBox = Label(window)
 
 
-
 #Configure & format widgets
 window.config(bg = bgColor)
-label.config(bg = bgColor, fg = textColor, font = smallFont)
-entry.config(bg = bgColor, font = smallFont)
-btn.config(bg = bgColor, font = smallFont)
-pwBox.config(bg = bgColor, font = smallFont)
+label.config(bg = bgColor, fg = textColor, font = avenir)
+entry.config(bg = bgColor, font = avenir)
+btn.config(bg = bgColor, font = avenir)
+pwBox.config(bg = bgColor, font = avenir)
+upperCaseCheckBox.config(bg = bgColor, font = avenir)
+numberCheckBox.config(bg = bgColor, font = avenir)
+symbolCheckBox.config(bg = bgColor, font = avenir)
 
 #Add all to window
-label.pack(ipady = 5)
-entry.pack()
-btn.pack(ipady = 5, ipadx = 5, pady = 5)
+upperCaseCheckBox.grid(row = 0, sticky = W)
+numberCheckBox.grid(row = 1, sticky = W)
+symbolCheckBox.grid(row = 2, sticky = W)
+label.grid(row = 3, sticky = W)
+entry.grid(row = 3, column = 1)
+btn.grid(row = 4, ipady = 5, ipadx = 5, pady = 5)
 
 window.mainloop()
